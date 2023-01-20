d18d89ddb1d93d4e243a7902f0366cf3f1dea8fd
OAtulA <oraonatula@gmail.com>
2023-01-21 00:48:33 +0530

    Another day of code another leetcode problem.

---
+++ 20-Jan-2023/Palindrome.java
@@ -0,0 +1,38 @@
+
+class Solution {
+    public static boolean isPalindrome(int x) {        
+
+        //We have to find palindrome;
+        if (x<0){
+            return false;
+        }
+        else{
+            StringBuffer original, reverse;
+            original = new StringBuffer( Integer.toString(x));
+            System.out.println(original);
+            reverse = original.reverse();
+            System.out.println(reverse);
+            if(original.equals(reverse)){
+                return true;                
+            }
+            else{
+                return false;
+            }
+        }        
+    }   
+    
+    public static void main(String[] args) {
+        boolean bi= isPalindrome(10);
+    }
+}
+
+/*
+class Palindrome extends Solution{
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter n: ");
+        int n = Integer.parseInt(br.readLine());
+        boolean b= isPalindrome(n);
+    }
+}
+ */
