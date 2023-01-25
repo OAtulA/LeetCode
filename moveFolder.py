6ba167017407ce4fc3973533ae9de2b89a2f3c55
OAtulA <oraonatula@gmail.com>
2023-01-25 23:57:16 +0530

    Restructured the folder structure.

---
+++ moveFolder.py
@@ -0,0 +1,10 @@
+import os
+
+Path = os.getcwd()
+files = os.listdir()
+
+for file in files:
+    if "-Jan-" in file:
+        jan_folers= os.path.abspath(file)
+        new_jan_folders = os.path.join(Path,"Jan/"+file)
+        os.replace(file, new_jan_folders)
