956c2be6a3cbd504948878ff44e36dcfbc8162fb
OAtulA <oraonatula@gmail.com>
2023-02-02 23:34:22 +0530

    Tried adding a C++ solution 11-Container-WIth-Most-Water/Solution.cpp

--- 2023/Feb/Feb -1/11-Container-WIth-Most-Water/Solution.cpp
+++ 2023/Feb/Feb -1/11-Container-WIth-Most-Water/Solution.cpp
@@ -5,17 +5,21 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int right = 0;
-        int left =  height.size()-1 ;
+        int right = 0,left =  height.size()-1 ;
         int maxArea =0;
         while(right>left){
-            int h = min(height[right], height[right]);
+            int h = min(height[right], height[left]);
             int w= right- left;
             int area= h*w;
             maxArea = max(area, maxArea);
             if( (height[left] > height[right]) )           
              right--;
-            else left++;
+            else if (height[left] < height[right])
+            left++;
+            else{
+                right--;
+                left++;
+            }
         }
         return maxArea;
     }
@@ -23,7 +27,9 @@ public:
 
 int main(int argc, char const *argv[])
 {
-    vector<int> height= {1,1};
+    vector<int> height;
+    // height= {1,1};
+    height = {1,8,6,2,5,4,8,3,7};
     Solution obj;
     cout<<obj.maxArea(height)<<endl;
     return 0;
