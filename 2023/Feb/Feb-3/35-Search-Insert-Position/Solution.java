1dec3a9ccd335e200a8cca24c9fc903d84f65b1a
OAtulA <oraonatula@gmail.com>
2023-02-04 00:03:04 +0530

    Made some basic changes to suit the answer.

--- 2023/Feb/Feb-3/35-Search-Insert-Position/Solution.java
+++ 2023/Feb/Feb-3/35-Search-Insert-Position/Solution.java
@@ -1,5 +1,7 @@
+// import javax.sound.midi.Soundbank;
+
 class Solution {
-    public int searchInsert(int[] nums, int target) {
+    public static int searchInsert(int[] nums, int target) {
         int start =0, end =  nums.length;
         int mid = (start+ end)/2;
 
@@ -9,7 +11,17 @@ class Solution {
             else if(nums[mid]> target)
                 end = mid -1;
             else 
-                
+                start = mid+1;
+            
+            mid = (start+ end)/2;
         }
+        if( mid != nums.length-1)
+         mid++;
+        
+         return mid;
+    }
+    public static void main(String[] args) {
+        int[] n = {1,3,5,6};
+        System.out.println(searchInsert(n, 2));
     }
 }
