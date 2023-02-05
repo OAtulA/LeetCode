30a8c1a6c8ffbe2a89d8bad3e73bd41a172efee9
OAtulA <oraonatula@gmail.com>
2023-02-05 19:58:00 +0530

    It isn't the answer I was looking for. I will start from prev ious version.

--- 2023/Feb/Feb-5/24-Swap-Nodes-in-Pairs/README.md
+++ 2023/Feb/Feb-5/24-Swap-Nodes-in-Pairs/README.md
@@ -2,7 +2,7 @@
 
 ## Approach1
 
-### Commit id
+### Commit id 3654586
 
 I will have 2 pointers.  
 itr and temp
@@ -10,3 +10,6 @@ I will pick 2 at a time and swap them.
 Next I will move the itr to next of temp.  
 
 My cpp solution didn't work and Java solution gave TLE.  
+
+### Commit id 
+
--- 2023/Feb/Feb-5/24-Swap-Nodes-in-Pairs/Solution.java
+++ 2023/Feb/Feb-5/24-Swap-Nodes-in-Pairs/Solution.java
@@ -1,26 +1,64 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+//
+  // Definition for singly-linked list.
+   class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ //
 
 class Solution {
-    public ListNode swapPairs(ListNode head) {
+    public static ListNode swapPairs(ListNode head) {
         ListNode itr = head,temp;
+        ListNode nextStart;
 
-        while (itr.next!= null) {
+        // Here itr is to iterate, temp to have 2nd immediate node
+        // nextStart is for next start point
+        try{
+        while (itr.next!= null && itr != null) {
+            
             temp = itr;
             itr = itr.next;
+            nextStart = itr.next;
             itr.next = temp;
+
+            temp.next =  nextStart;
             //Move to next pair
-            itr = itr.next.next;
+            itr = nextStart;
         }
-        
+        }
+        catch (NullPointerException e){
+             int flag =1 ;
+        }
         return head;
     }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode tail= head;
+
+        for (int i = 2; i <= 4; i++) {
+            ListNode node = new ListNode(i);
+            if(i==2)
+            {
+                tail = node;
+                head.next = tail;
+            }
+            else{
+                tail.next = node;                        
+                tail = node;
+            }                
+        }
+        ListNode ans = swapPairs(head);
+        System.out.println("ans: [");
+        while (ans.next != null) {
+            System.out.print(" "+ans.val);
+        }
+        System.out.println(" ]");
+        // System.out.println("ans: "+ Listnode.toString());
+    }
+
+
 }
