59b511b17a57c2bf22f409d268f7feba6950c58c
OAtulA <oraonatula@gmail.com>
2023-02-07 23:42:58 +0530

    modified:   24-Swap-Nodes-in-Pairs/Solution.java resolved the merge conflict.

--- 2023/Feb/Feb-5/24-Swap-Nodes-in-Pairs/Solution.java
+++ 2023/Feb/Feb-5/24-Swap-Nodes-in-Pairs/Solution.java
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-//
-  // Definition for singly-linked list.
-   class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
- //
-
-class Solution {
-    public static ListNode swapPairs(ListNode head) {
-        ListNode itr = head,temp;
-        ListNode nextStart;
-
-        // Here itr is to iterate, temp to have 2nd immediate node
-        // nextStart is for next start point
-        try{
-        while (itr.next!= null && itr != null) {
-            
-            temp = itr;
-            itr = itr.next;
-            nextStart = itr.next;
-            itr.next = temp;
-
-            temp.next =  nextStart;
-            //Move to next pair
-            itr = nextStart;
-        }
-        }
-        catch (NullPointerException e){
-             int flag =1 ;
-        }
-        return head;
-=======
 
 // Definition for singly-linked list.
 class ListNode {
@@ -88,26 +51,10 @@ class Solution {
         
 
         return dummy.next;
->>>>>>> better_approach
     }
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
-<<<<<<< HEAD
-        ListNode tail= head;
-
-        for (int i = 2; i <= 4; i++) {
-            ListNode node = new ListNode(i);
-            if(i==2)
-            {
-                tail = node;
-                head.next = tail;
-            }
-            else{
-                tail.next = node;                        
-                tail = node;
-            }                
-=======
         ListNode tail = head;
 
         for (int i = 2; i <= 4; i++) {
@@ -119,25 +66,15 @@ class Solution {
                 tail.next = node;
                 tail = node;
             }
->>>>>>> better_approach
         }
         ListNode ans = swapPairs(head);
         System.out.println("ans: [");
         while (ans.next != null) {
-<<<<<<< HEAD
-            System.out.print(" "+ans.val);
-=======
             System.out.print(" " + ans.val);
             ans = ans.next;
->>>>>>> better_approach
         }
         System.out.println(" ]");
         // System.out.println("ans: "+ Listnode.toString());
     }
 
-<<<<<<< HEAD
-
 }
-=======
-}
->>>>>>> better_approach
